b2986ee82232b44ef82432f0bc528b3bb22d1da3
GridIland <koussougboss@gmail.com>
2024-06-13 01:30:10 +0000

    Chapter 3 C1 resolution

--- exogo/main.go
+++ exogo/main.go
@@ -1,18 +1,33 @@
 package main
 
-func yearsUntilEvents(age int) (yearsUntilAdult, yearsUntilDrinking,yearsUntilCarRental int) {
-	yearsUntilAdult = 18 - age
-	if yearsUntilAdult < 0 {
-		yearsUntilAdult = 0
-	}
-	yearsUntilDrinking = 21 - age
-	if yearsUntilDrinking < 0 {
-		yearsUntilDrinking = 0
-	}
-	yearsUntilCarRental = 25 - age
-	if yearsUntilCarRental < 0 {
-		yearsUntilCarRental = 0
-	}
-	return
+import "fmt"
+
+func main (){
+  thisCost := 3
+  numness := 7421 
+  fmt.Print(calculateFinalBill(float64(thisCost),numness))
+}
+func calculateFinalBill(costpermessage float64, nummessages int) float64 {
+  baseBill := calculateBaseBill(costpermessage,nummessages)   
+  amountDeducted := baseBill * calculatediscount(nummessages)
+  fmt.Print(amountDeducted)
+  return baseBill - amountDeducted 
+}
+
+func calculatediscount(messagessent int) float64 {
+  if messagessent >1000 && messagessent<5000{
+    return .1
+  }
+  if messagessent >5000{
+    return .2
+  }else{
+    return 0.0
+  }
+}
+
+// don't touch below this line
+
+func calculateBaseBill(costPerMessage float64, messagesSent int) float64 {
+  return costPerMessage * float64(messagesSent)
 }
 
--- exogo/main_test.go
+++ exogo/main_test.go
@@ -7,54 +7,45 @@ import (
 
 func Test(t *testing.T) {
 	type testCase struct {
-		age                   int
-		exYearsUntilAdult     int
-		exYearsUntilDrinking  int
-		exYearsUntilCarRental int
+		costPerMessage float64
+		numMessages    int
+		expected       float64
 	}
 	tests := []testCase{
-		{4, 14, 17, 21},
-		{18, 0, 3, 7},
-		{22, 0, 0, 3},
+		{2.55, 89, 226.95},
+		{2.25, 204, 459},
+		{1, 1428, 1285.2},
+		{5, 1000, 5000},
+		{5, 1001, 4504.5},
 	}
 	if withSubmit {
 		tests = append(tests, []testCase{
-			{25, 0, 0, 0},
-			{35, 0, 0, 0},
+			{3, 0, 0},
+			{3, 7421, 17810.4},
 		}...)
 	}
 
 	for _, test := range tests {
-		if yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental := yearsUntilEvents(
-			test.age,
-		); yearsUntilAdult != test.exYearsUntilAdult ||
-			yearsUntilDrinking != test.exYearsUntilDrinking ||
-			yearsUntilCarRental != test.exYearsUntilCarRental {
+		if output := calculateFinalBill(
+			test.costPerMessage,
+			test.numMessages,
+		); output != test.expected {
 			t.Errorf(
-				"Test Failed: (%v) -> expected: (%v, %v, %v) actual: (%v, %v, %v)",
-				test.age,
-				test.exYearsUntilAdult,
-				test.exYearsUntilDrinking,
-				test.exYearsUntilCarRental,
-				yearsUntilAdult,
-				yearsUntilDrinking,
-				yearsUntilCarRental,
+				"Test Failed: (%v, %v) -> expected: %v actual: %v",
+				test.costPerMessage,
+				test.numMessages,
+				test.expected,
+				output,
 			)
 		} else {
-			fmt.Printf("Test Passed: (%v) -> expected: (%v, %v, %v) actual: (%v, %v, %v)\n",
-				test.age,
-				test.exYearsUntilAdult,
-				test.exYearsUntilDrinking,
-				test.exYearsUntilCarRental,
-				yearsUntilAdult,
-				yearsUntilDrinking,
-				yearsUntilCarRental,
+			fmt.Printf("Test Passed: (%v, %v) -> expected: %v actual: %v\n",
+				test.costPerMessage,
+				test.numMessages,
+				test.expected,
+				output,
 			)
 		}
 	}
 }
 
-// withSubmit is set at compile time depending
-// on which button is used to run the tests
-var withSubmit = true
-
+var withSubmit = true 
