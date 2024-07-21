9ee791379e6f94c9fe338a1be5c60429d90846a4
GridIland <koussougboss@gmail.com>
2024-07-21 22:42:39 +0000

    anonymous functions

--- revision/mainrevision.go
+++ revision/mainrevision.go
@@ -1,5 +1,32 @@
 package main
 
-func reformat(message string, formatter func(string) string) string {
-  return "TEXTIO: "+formatter(formatter(formatter(message)))
+import "fmt"
+
+func printReports(intro, body, outro string) {
+  printCostReport(func(s string) int {
+    return len(s)*2
+  },intro)
+  printCostReport(func(s string) int {
+    return len(s)*3
+  },body)
+  printCostReport(func(s string) int {
+    return len(s)*4
+  },outro)
 }
+
+// don't touch below this line
+
+func main() {
+	printReports(
+		"Welcome to the Hotel California",
+		"Such a lovely place",
+		"Plenty of room at the Hotel California",
+	)
+}
+
+func printCostReport(costCalculator func(string) int, message string) {
+	cost := costCalculator(message)
+	fmt.Printf(`Message: "%s" Cost: %v cents`, message, cost)
+	fmt.Println()
+}
+
