e8677077f3913617fc4a0551278e016e6cc2a69b
GridIland <koussougboss@gmail.com>
2024-07-22 22:58:26 +0000

    nested function

--- revision/mainreevision_test.go
+++ revision/mainreevision_test.go
@@ -2,62 +2,78 @@ package main
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 )
 
 func Test(t *testing.T) {
 	type testCase struct {
-		productID      string
-		quantity       int
-		accountBalance float64
-		expected_1     bool
-		expected_2     float64
+		input    []int
+		expected []int
 	}
 	tests := []testCase{
-		{"1", 2, 226.95, true, 223.95},
-		{"2", 4, 459, true, 450},
-		{"3", 4, 1185.2, true, 1173.2},
-		{"4", 5, 0, false, 0},
-		{"5", 50, 195, true, 70},
+		{
+			input:    []int{1, 2, 3},
+			expected: []int{1, 3, 6},
+		},
+		{
+			input:    []int{1, 2, 3, 4, 5},
+			expected: []int{1, 3, 6, 10, 15},
+		},
 	}
+
 	if withSubmit {
 		tests = append(tests, []testCase{
-			{"6", 0, 100, true, 100},
-			{"7", 7421, 210.24, false, 210.24},
-			{"8", 55, 24.5, false, 24.5},
-			{"9", 1, 999.99, true, 0},
+			{
+				input:    []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+				expected: []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55},
+			},
+			{
+				input:    []int{0, 0, 0, 0},
+				expected: []int{0, 0, 0, 0},
+			},
+			{
+				input:    []int{5, -3, -1},
+				expected: []int{5, 2, 1},
+			},
 		}...)
 	}
 
 	for _, test := range tests {
-		if output_1, output_2 := placeOrder(
-			test.productID,
-			test.quantity,
-			test.accountBalance,
-		); output_1 != test.expected_1 || output_2 != test.expected_2 {
-			t.Errorf(
-				"Test Failed: (%v, %v, %v) -> expected: (%v, %v) actual: (%v, %v)\n",
-				test.productID,
-				test.quantity,
-				test.accountBalance,
-				test.expected_1,
-				test.expected_2,
-				output_1,
-				output_2,
+		f := adder()
+		results := make([]int, len(test.input))
+		for i, v := range test.input {
+			results[i] = f(v)
+		}
+		if !slices.Equal(results, test.expected) {
+			t.Errorf(`
+Test Failed.
+  input: %v
+->
+  expected: %v
+  actual: %v
+`,
+				test.input,
+				test.expected,
+				results,
 			)
 		} else {
-			fmt.Printf("Test Passed: (%v, %v, %v) -> expected: (%v, %v) actual: (%v, %v)\n",
-				test.productID,
-				test.quantity,
-				test.accountBalance,
-				test.expected_1,
-				test.expected_2,
-				output_1,
-				output_2,
+			fmt.Printf(`
+Test Passed.
+  input: %v
+->
+  expected: %v
+  actual: %v
+			`,
+				test.input,
+				test.expected,
+				results,
 			)
 		}
 	}
 }
 
+// withSubmit is set at compile time depending
+// on which button is used to run the tests
 var withSubmit = true
 
