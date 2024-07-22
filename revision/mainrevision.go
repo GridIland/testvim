2ae01900b1d87352eb26a6ea7053f9c4e3104fa9
GridIland <koussougboss@gmail.com>
2024-07-22 00:44:48 +0000

    being chilling

--- revision/mainrevision.go
+++ revision/mainrevision.go
@@ -1,22 +1,67 @@
 package main
 
-func calculateFinalBill(costPerMessage float64, numMessages int) float64 {
-  return calculateBaseBill(costPerMessage, numMessages) - calculateBaseBill(costPerMessage, numMessages)*calculateDiscount(numMessages)
+func placeOrder(productID string, quantity int, accountBalance float64) (bool, float64) {
+  if quantity > amountInStock(productID){
+    return false, accountBalance
+  }
+  if priceList(productID)*float64(quantity) > accountBalance{ 
+    return false, accountBalance
+  }
+  
+  return true, accountBalance - priceList(productID)*float64(quantity)
 }
 
-func calculateDiscount(messagesSent int) float64 {
-  if messagesSent > 5000{
-    return .2
-  }
-  if messagesSent > 1000{
-    return .1
-  }
-  return 0
+// Don't touch below this line
+
+func calcPrice(productID string, quantity int) float64 {
+	return priceList(productID) * float64(quantity)
 }
 
-// don't touch below this line
+func priceList(productID string) float64 {
+	if productID == "1" {
+		return 1.50
+	} else if productID == "2" {
+		return 2.25
+	} else if productID == "3" {
+		return 3.00
+	} else if productID == "4" {
+		return 1.00
+	} else if productID == "5" {
+		return 2.50
+	} else if productID == "6" {
+		return 8.99
+	} else if productID == "7" {
+		return 22.50
+	} else if productID == "8" {
+		return 50.00
+	} else if productID == "9" {
+		return 999.99
+	} else {
+		return 0.00
+	}
+}
 
-func calculateBaseBill(costPerMessage float64, messagesSent int) float64 {
-	return costPerMessage * float64(messagesSent)
+func amountInStock(productID string) int {
+	if productID == "1" {
+		return 11
+	} else if productID == "2" {
+		return 25
+	} else if productID == "3" {
+		return 4
+	} else if productID == "4" {
+		return 6
+	} else if productID == "5" {
+		return 50
+	} else if productID == "6" {
+		return 2
+	} else if productID == "7" {
+		return 0
+	} else if productID == "8" {
+		return 99
+	} else if productID == "9" {
+		return 1
+	} else {
+		return 0
+	}
 }
 
